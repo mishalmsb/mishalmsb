55d744e1ac43465d682dd7ebe982ace99a4275f4
Mishal Shashikant <mishalmsb@gmail.com>
2022-02-22 20:34:15 +0000

    replace libraries to use plain javascript to print cv

--- themes/version-1/assets/js/cv.js
+++ themes/version-1/assets/js/cv.js
@@ -1,57 +1,32 @@
-// const cvEn = document.querySelector(".cv-en");
-// const cvPt = document.querySelector(".cv-pt");
-// var activeCvLang = "en";
-
-// function showCVByLang(lang) {
-//   activeCvLang = lang;
-//   if (activeCvLang == "pt") {
-//     cvEn.classList.add("d-none");
-//     cvPt.classList.remove("d-none");
-//   } else {
-//     cvEn.classList.remove("d-none");
-//     cvPt.classList.add("d-none");
-//   }
-// }
-
-// function printCv() {
-//   var el = document.querySelector(`.cv-${activeCvLang}`);
-
-//   console.log(el);
-
-//   html2pdf(el, {
-//     filename: "myfile.pdf",
-//     image: { type: "jpeg", quality: 1 },
-//     html2canvas: { scale: 2, logging: true, letterRendering: true },
-//     jsPDF: {
-//       unit: "in",
-//       format: "a4",
-//       orientation: "portrait",
-//       pagesplit: true,
-//     },
-//   });
-// }
-
-// function printCv(lang = "en") {
-//   var el = document.querySelector(`.cv-${lang}`);
-//   var filename = lang == "en" ? "cv-mishal.pdf" : "cv-mishal-pt.pdf";
-//   html2pdf(el, {
-//     filename: filename,
-//     image: { type: "jpeg", quality: 1 },
-//     html2canvas: { scale: 2, logging: true, letterRendering: true },
-//     jsPDF: {
-//       unit: "in",
-//       format: "letter",
-//       orientation: "portrait",
-//       pagesplit: true,
-//     },
-//   });
-// }
-
 function printCv(lang = "en") {
-  var el = document.querySelector(`.cv-${lang}`);
+  // document.body.classList.add("printing");
 
-  var pdf = new jsPDF("p", "pt", "a4");
-  pdf.addHTML(el, 15, 15, function () {
-    pdf.save("div.pdf");
-  });
+  // setTimeout(() => {
+  //   window.print();
+  // }, 1000);
+  window.print();
 }
+
+var beforePrint = function () {
+  console.log("Functionality to run before printing.");
+  document.body.classList.add("printing");
+};
+
+var afterPrint = function () {
+  console.log("Functionality to run after printing");
+  document.body.classList.remove("printing");
+};
+
+// if (window.matchMedia) {
+//   var mediaQueryList = window.matchMedia("print");
+//   mediaQueryList.addListener(function (mql) {
+//     if (mql.matches) {
+//       beforePrint();
+//     } else {
+//       afterPrint();
+//     }
+//   });
+// }
+
+window.onbeforeprint = beforePrint;
+window.onafterprint = afterPrint;
